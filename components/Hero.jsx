ce3405f53f3b345f211ca173e0538412f3aa07d3
Ahmad <scriptwithahmad@gmail.com>
2023-08-29 17:56:15 +0500

    edify.pk Home Page

---
+++ components/Hero.jsx
@@ -0,0 +1,47 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// import required modules
+import { EffectFade, Navigation } from "swiper/modules";
+
+export default function App() {
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        effect={"fade"}
+        navigation={true}
+        modules={[EffectFade, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide className="slide slide-01">
+          <div className="slideContent">
+            <span>Educal University</span>
+            <h2>Together We'll <br /> Explore New Things</h2>
+            <p>
+              We believe everyone should have the opportunity to create progress
+              through technolog.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="slide slide-02">
+          <div className="slideContent">
+            <span>Educal University</span>
+            <h2 className="slider__title">Institution of <br /> Higher Education</h2>
+            <p>
+              We believe everyone should have the opportunity to create progress
+              through technolog.
+            </p>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    DB_URI:
+    "mongodb+srv://ahmed:ahmed@edify.9anuaq1.mongodb.net/team?retryWrites=true&w=majority",
+    NEXTAUTH_SECRET: "CodewithAhmad",
+  },
+};
+
+module.exports = nextConfig;
